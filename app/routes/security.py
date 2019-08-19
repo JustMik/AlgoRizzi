136eb27689f81c37569f0dfc00c9386e329fb097
JustMik <justin.rigon.92@gmail.com>
2019-08-19 16:57:30 +0200

    Modify Login Structure - Wrap security login template

--- app/routes/security.py
+++ app/routes/security.py
@@ -2,6 +2,4 @@ from flask import Blueprint, render_template, session,abort
 
 security_route = Blueprint('sec',__name__)
 
-@security_route.route('/login/', methods=['GET', 'POST'])
-def login():
-    return render_template('public/login.html')
+
